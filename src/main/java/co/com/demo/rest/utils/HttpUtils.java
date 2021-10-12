73eba4de3d2b06b1f1a42bd7ed0e1bff6089ccf7
daagalle <daagalle@bancolombia.com.co>
2021-10-11 20:39:11 -0500

    updated README.md

--- src/main/java/co/com/demo/rest/utils/HttpUtils.java
+++ src/main/java/co/com/demo/rest/utils/HttpUtils.java
@@ -7,7 +7,6 @@ public final class HttpUtils {
     public static final String CONTENT_TYPE = "Content-Type";
     public static final String ACCEPT = "Accept";
     public static final String AUTHORIZATION = "Authorization";
-    public static final String ADMIN = "admin";
 
     private HttpUtils() {
         //final class
